82852062d8ee3cab275cb384ebce4198694c196e
David Stöter <113838717+DStoeter@users.noreply.github.com>
2023-02-01 12:10:32 +0100

    Update FEDformer.py

--- models/FEDformer.py
+++ models/FEDformer.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
 from layers.Embed import DataEmbedding, DataEmbedding_wo_pos
 from layers.AutoCorrelation import AutoCorrelation, AutoCorrelationLayer
 from layers.FourierCorrelation import FourierBlock, FourierCrossAttention
@@ -9,7 +10,6 @@ from layers.SelfAttention_Family import FullAttention, ProbAttention
 from layers.Autoformer_EncDec import Encoder, Decoder, EncoderLayer, DecoderLayer, my_Layernorm, series_decomp, series_decomp_multi
 import math
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -116,8 +116,6 @@ class Model(nn.Module):
             projection=nn.Linear(configs.d_model, configs.c_out, bias=True)
         )
 
-
-
     @staticmethod
     def get_rand_mask(shape, mu=5, std=3):
         batch_size = shape[0]
@@ -129,8 +127,7 @@ class Model(nn.Module):
         if span_size < 1:
             span_size = 1
         n_spans = int(len_ds / span_size)
-        #         print(span_size)
-        mask = np.random.choice([False, True], [batch_size, n_spans, feature_dim], p=[0.8, 0.2])
+        mask = np.random.choice([False, True], [batch_size, n_spans, feature_dim], p=[0.90, 0.10])
 
         if not np.all(np.any(mask, axis=1)):
             rand_true = np.random.randint(n_spans - 1)
@@ -141,158 +138,144 @@ class Model(nn.Module):
         mask = np.concatenate([mask, ones], axis=1)
         return torch.tensor(mask, dtype=bool)
 
-    def mask_seasonal_init(self, seasonal_init):
+    def mask_init(self, seasonal_init):
 
-            seasonal_init_tmp = seasonal_init.detach().cpu().numpy()
-            seasonal_init_tmp = torch.tensor(seasonal_init_tmp)
-            shape = seasonal_init.shape
-            mask = self.get_rand_mask(shape)
-            zeros = torch.zeros(seasonal_init.shape, dtype=seasonal_init.dtype)
-
-            seasonal_init_masked = torch.where(mask, seasonal_init_tmp, zeros)
-
-            seasonal_init_masked = seasonal_init_masked.float().to(device)
-
-            mask = mask.to(device)
-            dec_input = seasonal_init * mask
-
-            return torch.tensor(dec_input)
-
-    def encoder_mask(self, batch_x_f):
-        batch_x_tmp = batch_x_f.detach().cpu().numpy()
-        batch_x_tmp = torch.tensor(batch_x_tmp)
-        shape = batch_x_f.shape
+        seasonal_init_tmp = seasonal_init.detach().cpu().numpy()
+        seasonal_init_tmp = torch.tensor(seasonal_init_tmp)
+        shape = seasonal_init.shape
         mask = self.get_rand_mask(shape)
-        zeros = torch.zeros(batch_x_f.shape, dtype=batch_x_f.dtype)
+        zeros = torch.zeros(seasonal_init.shape, dtype=seasonal_init.dtype)
 
-        batch_x_masked = torch.where(mask, batch_x_tmp, zeros)
+        seasonal_init_masked = torch.where(mask, seasonal_init_tmp, zeros)
 
-        batch_x_masked = batch_x_masked.float().to(device)
+        seasonal_init_masked = seasonal_init_masked.float().to(device)
 
         mask = mask.to(device)
-        enc_input = batch_x_f * mask
+        dec_input = seasonal_init * mask
 
-        return torch.tensor(enc_input), mask
+        return torch.tensor(dec_input), mask
 
-    def decoder_mask(self, batch_x_f):
-        batch_x_tmp = batch_x_f.detach().cpu().numpy()
-        batch_x_tmp = torch.tensor(batch_x_tmp)
-        shape = batch_x_f.shape
-        mask = self.get_rand_mask(shape)
-        zeros = torch.zeros(batch_x_f.shape, dtype=batch_x_f.dtype)
+    def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, pre_train=False, batch_y=None,
+                enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None):
 
-        batch_x_masked = torch.where(mask, batch_x_tmp, zeros)
+        if pre_train:
+            mean = torch.mean(x_enc, dim=1).unsqueeze(1).repeat(1, 6, 1)
+            zeros = torch.zeros([x_dec.shape[0], self.pred_len, x_dec.shape[2]]).to(device)  # cuda()
 
-        batch_x_masked = batch_x_masked.float().to(device)
-
-        mask = mask.to(device)
-        enc_input = batch_x_f * mask
-
-        return torch.tensor(enc_input), mask
-
-
-    def forward(self, use_mask, master_mask, x_enc, x_mark_enc, x_dec, x_mark_dec,
-                enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None,):
-
-        # decomp init
-        mean = torch.mean(x_enc, dim=1).unsqueeze(1).repeat(1, self.pred_len, 1)
-        zeros = torch.zeros([x_dec.shape[0], self.pred_len, x_dec.shape[2]]).to(device)  # cuda()
-
-        if use_mask:
             seasonal_init, trend_init = self.decomp(x_enc)
-            seasonal_init = seasonal_init * master_mask
-            trend_init = trend_init * master_mask
-            x_dec_mask, mask_d = self.decoder_mask(x_dec)
-        else:
-            seasonal_init, trend_init = self.decomp(x_enc)
+            seasonal_init_y, trend_init_y = self.decomp(batch_y)
 
-        #shape = 32,96,1
+            trend_init = torch.cat([trend_init[:, :, :], trend_init_y[:, -42:, :]], dim=1)
+            seasonal_init = torch.cat([seasonal_init[:, :, :], seasonal_init_y[:, -42:, :]], dim=1)
 
-
-        # decoder input
-        trend_init = torch.cat([trend_init[:, -self.label_len:, :], mean], dim=1)
+            enc_out = self.enc_embedding(x_enc, x_mark_enc)
 
 
-        if use_mask:
-            seasonal_init = torch.cat([seasonal_init[:, :, :], x_dec_mask[:, self.pred_len:, :]], dim=1)
-        else:
-            seasonal_init = F.pad(seasonal_init[:, -self.label_len:, :], (0, 0, 0, self.pred_len))
-
-        #([32, 144, 1])
-
-        # enc
-        enc_out = self.enc_embedding(x_enc, x_mark_enc)
-        #([32, 96, 512])
-        if use_mask:
-            enc_out_masked, mask = self.encoder_mask(enc_out)
-            enc_out, attns = self.encoder(enc_out_masked, attn_mask=enc_self_mask)
-        else:
             enc_out, attns = self.encoder(enc_out, attn_mask=enc_self_mask)
 
+            # seasonal_init, mask = self.mask_init(seasonal_init)
+            # trend_init = trend_init * mask
 
-        #([32, 96, 512])
+            # print("Seasonal and Trend plot after masking")
+            # seasonal_init_plt = seasonal_init.detach().cpu()
+            # seasonal_init_plt_y = seasonal_init_y.detach().cpu()
+            # trend_init_plt = trend_init.detach().cpu()
+            # plt.rcParams["figure.dpi"] = 100
+            # plt.rcParams['lines.linewidth'] = 0.4
+            # plt.plot(seasonal_init_plt[0], label='Seasonal')
+            # plt.plot(seasonal_init_plt_y[0], label='Seasonal y')
+            # plt.plot(trend_init_plt[0], label='Trend')
+            # plt.legend()
+            # plt.show()
 
-        # dec
-        dec_out = self.dec_embedding(seasonal_init, x_mark_dec)
-        #([32, 144, 512])
+            dec_out = self.dec_embedding(seasonal_init, x_mark_dec)
 
-        seasonal_part, trend_part = self.decoder(dec_out, enc_out, x_mask=dec_self_mask, cross_mask=dec_enc_mask,
-                                                 trend=trend_init)
-        #([32, 144, 1])
+            dec_out, mask = self.mask_init(dec_out)
 
-        # final
-        dec_out = trend_part + seasonal_part
-        #([32, 144, 1])
+            seasonal_part, trend_part = self.decoder(dec_out, enc_out, x_mask=dec_self_mask, cross_mask=dec_enc_mask,
+                                                     trend=trend_init)
 
-        if use_mask:
+            dec_out = trend_part + seasonal_part
+
             if self.output_attention:
-                return dec_out[:, :, :],seasonal_init, attns
+                return dec_out[:, :, :], attns
             else:
-                return dec_out[:, :, :], seasonal_init  # [B, L, D]
+                return dec_out[:, :, :]  # [B, L, D]
+
         else:
+            # decomp init
+            mean = torch.mean(x_enc, dim=1).unsqueeze(1).repeat(1, self.pred_len, 1)
+            zeros = torch.zeros([x_dec.shape[0], self.pred_len, x_dec.shape[2]]).to(device)  # cuda()
+            seasonal_init, trend_init = self.decomp(x_enc)
+            seasonal_init = F.pad(seasonal_init[:, -self.label_len:, :], (0, 0, 0, self.pred_len))
+            trend_init = torch.cat([trend_init[:, -self.label_len:, :], mean], dim=1)
+
+            # enc
+            enc_out = self.enc_embedding(x_enc, x_mark_enc)
+
+            enc_emb = enc_out
+
+            enc_out, mask = self.mask_init(enc_out)
+
+            enc_out, attns = self.encoder(enc_out, attn_mask=enc_self_mask)
+
+            enc_out_loss = enc_out
+            # enc = 32, 96, 512
+            # dec
+            dec_out = self.dec_embedding(seasonal_init, x_mark_dec)
+            seasonal_part, trend_part = self.decoder(dec_out, enc_out, x_mask=dec_self_mask, cross_mask=dec_enc_mask,
+                                                     trend=trend_init)
+            # dec = 32, 144, 512
+            # seasonal_part = 32, 144, 1
+            # trend_part = 32, 144, 1
+
+            # final
+            dec_out = trend_part + seasonal_part
+
             if self.output_attention:
                 return dec_out[:, -self.pred_len:, :], attns
             else:
-                return dec_out[:, -self.pred_len:, :]  # [B, L, D]
+                return dec_out[:, -self.pred_len:, :], enc_emb, enc_out_loss  # [B, L, D]
 
-    if __name__ == '__main__':
-        class Configs(object):
-            ab = 0
-            modes = 32
-            mode_select = 'random'
-            # version = 'Fourier'
-            version = 'Wavelets'
-            moving_avg = [12, 24]
-            L = 1
-            base = 'legendre'
-            cross_activation = 'tanh'
-            seq_len = 96
-            label_len = 48
-            pred_len = 96
-            output_attention = True
-            enc_in = 7
-            dec_in = 7
-            d_model = 16
-            embed = 'timeF'
-            dropout = 0.05
-            freq = 'h'
-            factor = 1
-            n_heads = 8
-            d_ff = 16
-            e_layers = 2
-            d_layers = 1
-            c_out = 7
-            activation = 'gelu'
-            wavelet = 0
 
-        configs = Configs()
-        model = Model(configs)
+if __name__ == '__main__':
+    class Configs(object):
+        ab = 0
+        modes = 32
+        mode_select = 'random'
+        version = 'Fourier'
+        # version = 'Wavelets'
+        moving_avg = [12, 24]
+        L = 1
+        base = 'legendre'
+        cross_activation = 'tanh'
+        seq_len = 96
+        label_len = 48
+        pred_len = 96
+        output_attention = True
+        enc_in = 7
+        dec_in = 7
+        d_model = 16
+        embed = 'timeF'
+        dropout = 0.05
+        freq = 'h'
+        factor = 1
+        n_heads = 8
+        d_ff = 16
+        e_layers = 2
+        d_layers = 1
+        c_out = 7
+        activation = 'gelu'
+        wavelet = 0
 
-        print('parameter number is {}'.format(sum(p.numel() for p in model.parameters())))
-        enc = torch.randn([3, configs.seq_len, 7])
-        enc_mark = torch.randn([3, configs.seq_len, 4])
+    configs = Configs()
+    model = Model(configs)
 
-        dec = torch.randn([3, configs.seq_len // 2 + configs.pred_len, 7])
-        dec_mark = torch.randn([3, configs.seq_len // 2 + configs.pred_len, 4])
-        out = model.forward(enc, enc_mark, dec, dec_mark)
-        print(out)
+    print('parameter number is {}'.format(sum(p.numel() for p in model.parameters())))
+    enc = torch.randn([3, configs.seq_len, 7])
+    enc_mark = torch.randn([3, configs.seq_len, 4])
+
+    dec = torch.randn([3, configs.seq_len//2+configs.pred_len, 7])
+    dec_mark = torch.randn([3, configs.seq_len//2+configs.pred_len, 4])
+    out = model.forward(enc, enc_mark, dec, dec_mark)
+    print(out)
