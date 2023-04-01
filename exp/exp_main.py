6e4359fdce661acc633bc9d777de3088174133d1
David Stöter <113838717+DStoeter@users.noreply.github.com>
2023-04-01 16:59:54 +0200

    Update exp_main.py

--- exp/exp_main.py
+++ exp/exp_main.py
@@ -33,8 +33,8 @@ class Exp_Main(Exp_Basic):
             model = nn.DataParallel(model, device_ids=self.args.device_ids)
         return model
 
-    def _get_data(self, flag):
-        data_set, data_loader = data_provider(self.args, flag)
+    def _get_data(self, flag, pre_train):
+        data_set, data_loader = data_provider(self.args, flag, pre_train)
         return data_set, data_loader
 
     def _select_optimizer(self):
@@ -81,15 +81,63 @@ class Exp_Main(Exp_Basic):
                 loss = criterion(pred, true)
 
                 total_loss.append(loss)
+
         total_loss = np.average(total_loss)
         self.model.train()
         return total_loss
 
+    def pre_vali(self, vali_data, vali_loader, criterion):
+        use_mask = False
+
+        pre_train = True
+        pre_total_loss = []
+        self.model.eval()
+        with torch.no_grad():
+            for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(vali_loader):
+
+                time_series = torch.cat([batch_x[:, :, :], batch_y[:, -(self.args.pred_len - self.args.label_len):, :]], dim=1).float().to(self.device)
+
+                batch_x = batch_x.float().to(self.device)
+                batch_y = batch_y.float().to(self.device)
+
+
+
+                batch_x_mark = batch_x_mark.float().to(self.device)
+                batch_y_mark = batch_y_mark.float().to(self.device)
+
+                # decoder input
+                dec_inp = torch.zeros_like(batch_y[:, -self.args.pred_len:, :]).float()
+                dec_inp = torch.cat([batch_y[:, :self.args.label_len, :], dec_inp], dim=1).float().to(self.device)
+                # encoder - decoder
+                if self.args.use_amp:
+                    with torch.cuda.amp.autocast():
+                        if self.args.output_attention:
+                            outputs = self.model(batch_x, batch_x_mark, dec_inp,  batch_y_mark, pre_train, batch_y)[0]
+                        else:
+                            outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark, pre_train, batch_y)
+                else:
+                    if self.args.output_attention:
+                        outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark, pre_train, batch_y)[0]
+                    else:
+                        outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark, pre_train, batch_y)
+                f_dim = -1 if self.args.features == 'MS' else 0
+                batch_y = batch_y[:, -self.args.pred_len:, f_dim:].to(self.device)
+
+                pred = outputs.detach().cpu()
+                true = time_series.detach().cpu()
+
+                loss = criterion(pred, true)
+
+                pre_total_loss.append(loss)
+        pre_total_loss = np.average(pre_total_loss)
+        self.model.train()
+        return pre_total_loss
+
     def pre_train(self, setting):
 
-        train_data, train_loader = self._get_data(flag='train')
-        vali_data, vali_loader = self._get_data(flag='val')
-        test_data, test_loader = self._get_data(flag='test')
+        train_data, train_loader = self._get_data(flag='train', pre_train=1)
+        vali_data, vali_loader = self._get_data(flag='val', pre_train=1)
+        test_data, test_loader = self._get_data(flag='test', pre_train=0)
 
         pre_train = True
 
@@ -100,7 +148,7 @@ class Exp_Main(Exp_Basic):
         time_now = time.time()
 
         train_steps = len(train_loader)
-        early_stopping = EarlyStopping(patience=self.args.patience - 1, verbose=True)
+        early_stopping = EarlyStopping(patience=self.args.patience, verbose=True)
 
 
         model_optim = self._select_optimizer()
@@ -153,7 +201,18 @@ class Exp_Main(Exp_Basic):
 
 
                     f_dim = -1 if self.args.features == 'MS' else 0
-                    # batch_y = batch_y[:, -self.args.pred_len:, f_dim:].to(self.device)
+                    #batch_y = batch_y[:, -self.args.pred_len:, f_dim:].to(self.device)
+
+                    # print("Output and Time Series")
+                    # outputs_plt = outputs.detach().cpu()
+                    # time_series_plt = time_series.detach().cpu()
+                    # plt.rcParams["figure.dpi"] = 200
+                    # plt.rcParams['lines.linewidth'] = 0.4
+                    # plt.rcParams["figure.figsize"] = [10.50, 3.50]
+                    # #plt.plot(outputs_plt[0], label='outputs')
+                    # plt.plot(time_series_plt[0], label='Illness')
+                    # plt.legend()
+                    # plt.show()
 
                     loss = criterion(outputs, time_series)
 
@@ -179,8 +238,8 @@ class Exp_Main(Exp_Basic):
 
             print("Epoch: {} cost time: {}".format(epoch + 1, time.time() - epoch_time))
             pre_train_loss = np.average(pre_train_loss)
-            vali_loss = self.vali(vali_data, vali_loader, criterion)
-            test_loss = self.vali(test_data, test_loader, criterion)
+            vali_loss = self.pre_vali(vali_data, vali_loader, criterion)
+            test_loss = self.pre_vali(test_data, test_loader, criterion)
 
             print("Epoch: {0}, Steps: {1} | Train Loss: {2:.7f} Vali Loss: {3:.7f} Test Loss: {4:.7f}".format(
                 epoch + 1, train_steps, pre_train_loss, vali_loss, test_loss))
@@ -197,9 +256,9 @@ class Exp_Main(Exp_Basic):
         return self.model
 
     def train(self, setting):
-        train_data, train_loader = self._get_data(flag='train')
-        vali_data, vali_loader = self._get_data(flag='val')
-        test_data, test_loader = self._get_data(flag='test')
+        train_data, train_loader = self._get_data(flag='train', pre_train= 0)
+        vali_data, vali_loader = self._get_data(flag='val', pre_train= 0)
+        test_data, test_loader = self._get_data(flag='test', pre_train= 0)
 
         path = os.path.join(self.args.checkpoints, setting)
         if not os.path.exists(path):
@@ -259,7 +318,7 @@ class Exp_Main(Exp_Basic):
                     loss = criterion(outputs, batch_y)
                     train_loss.append(loss.item())
 
-                if (i + 1) % 100 == 0:
+                if (i + 1) % 50 == 0:
                     print("\titers: {0}, epoch: {1} | loss: {2:.7f}".format(i + 1, epoch + 1, loss.item()))
                     speed = (time.time() - time_now) / iter_count
                     left_time = speed * ((self.args.train_epochs - epoch) * train_steps - i)
@@ -296,11 +355,10 @@ class Exp_Main(Exp_Basic):
 
     def test(self, setting, test=0):
         use_mask = False
-        test_data, test_loader = self._get_data(flag='test')
+        test_data, test_loader = self._get_data(flag='test', pre_train=0)
         if test:
             print('loading model')
             self.model.load_state_dict(torch.load(os.path.join('./checkpoints/' + setting, 'checkpoint.pth')))
-
         preds = []
         trues = []
         folder_path = './test_results/' + setting + '/'
@@ -350,26 +408,26 @@ class Exp_Main(Exp_Basic):
                     pd = np.concatenate((input[0, :, -1], pred[0, :, -1]), axis=0)
                     visual(gt, pd, os.path.join(folder_path, str(i) + '.pdf'))
 
-        preds = np.array(preds)
-        trues = np.array(trues)
-        print('test shape:', preds.shape, trues.shape)
-        preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
-        trues = trues.reshape(-1, trues.shape[-2], trues.shape[-1])
-        print('test shape:', preds.shape, trues.shape)
+            preds = np.array(preds)
+            trues = np.array(trues)
+            print('test shape:', preds.shape, trues.shape)
+            preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
+            trues = trues.reshape(-1, trues.shape[-2], trues.shape[-1])
+            print('test shape:', preds.shape, trues.shape)
 
-        # result save
-        folder_path = './results/' + setting + '/'
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
+            # result save
+            folder_path = './results/' + setting + '/'
+            if not os.path.exists(folder_path):
+                os.makedirs(folder_path)
 
-        mae, mse, rmse, mape, mspe = metric(preds, trues)
-        print('mse:{}, mae:{}'.format(mse, mae))
-        f = open("result.txt", 'a')
-        f.write(setting + "  \n")
-        f.write('mse: {} mae: {}'.format(mse, mae))
-        f.write('\n')
-        f.write('\n')
-        f.close()
+            mae, mse, rmse, mape, mspe = metric(preds, trues)
+            print('mse:{}, mae:{}'.format(mse, mae))
+            f = open("result.txt", 'a')
+            f.write(setting + "  \n")
+            f.write('mse: {} mae: {}'.format(mse, mae))
+            f.write('\n')
+            f.write('\n')
+            f.close()
 
         np.save(folder_path + 'metrics.npy', np.array([mae, mse, rmse, mape, mspe]))
         np.save(folder_path + 'pred.npy', preds)
@@ -378,7 +436,7 @@ class Exp_Main(Exp_Basic):
         return
 
     def predict(self, setting, load=False):
-        pred_data, pred_loader = self._get_data(flag='pred')
+        pred_data, pred_loader = self._get_data(flag='pred', pre_train=0)
 
         if load:
             path = os.path.join(self.args.checkpoints, setting)
