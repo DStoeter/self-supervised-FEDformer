f07f91be33ac5a2f4a7e25fb25af57910e0ffd29
David Stöter <113838717+DStoeter@users.noreply.github.com>
2023-04-01 16:57:41 +0200

    Update run.py

--- run.py
+++ run.py
@@ -5,22 +5,21 @@ import torch
 from exp.exp_main import Exp_Main
 import random
 import numpy as np
-
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def main():
-    print("________________________________________________________________________")
-    print("________________________________________________________________________")
-    print("________________________________________________________________________")
     fix_seed = 2021
     random.seed(fix_seed)
     torch.manual_seed(fix_seed)
     np.random.seed(fix_seed)
+    pre_train = True
 
     parser = argparse.ArgumentParser(description='Autoformer & Transformer family for Time Series Forecasting')
 
     # basic config
     parser.add_argument('--is_training', type=int, default=1, help='status')
-    parser.add_argument('--task_id', type=str, default='test', help='task id')
+    parser.add_argument('--task_id', type=str, default='Illness_RandMask20_predLen60', help='task id')
     parser.add_argument('--model', type=str, default='FEDformer',
                         help='model name, options: [FEDformer, Autoformer, Informer, Transformer]')
 
@@ -33,13 +32,21 @@ def main():
     parser.add_argument('--L', type=int, default=3, help='ignore level')
     parser.add_argument('--base', type=str, default='legendre', help='mwt base')
     parser.add_argument('--cross_activation', type=str, default='tanh',
-                        help='mwt cross atention activation function tanh or softmax')
+                        help='mwt cross attention activation function tanh or softmax')
 
     # data loader
-    parser.add_argument('--data', type=str, default='ETTh1', help='dataset type')
-    parser.add_argument('--root_path', type=str, default='./dataset/ETT/', help='root path of the data file')
-    parser.add_argument('--data_path', type=str, default='ETTh1.csv', help='data file')
-    parser.add_argument('--features', type=str, default='M',
+    parser.add_argument('--data', type=str, default='custom', help='dataset type')
+    #parser.add_argument('--root_path', type=str, default='./dataset/traffic/', help='root path of the data file')
+    parser.add_argument('--root_path', type=str, default='./dataset/illness/', help='root path of the data file')
+    #parser.add_argument('--root_path', type=str, default='./dataset/exchange_rate/', help='root path of the data file')
+    #parser.add_argument('--root_path', type=str, default='./dataset/electricity/', help='root path of the data file')
+    #parser.add_argument('--root_path', type=str, default='./dataset/weather/', help='root path of the data file')
+    #parser.add_argument('--data_path', type=str, default='traffic.csv', help='data file')
+    parser.add_argument('--data_path', type=str, default='national_illness.csv', help='data file')
+    #parser.add_argument('--data_path', type=str, default='exchange_rate.csv', help='data file')
+    #parser.add_argument('--data_path', type=str, default='electricity.csv', help='data file')
+    #parser.add_argument('--data_path', type=str, default='weather.csv', help='data file')
+    parser.add_argument('--features', type=str, default='S',
                         help='forecasting task, options:[M, S, MS]; M:multivariate predict multivariate, '
                              'S:univariate predict univariate, MS:multivariate predict univariate')
     parser.add_argument('--target', type=str, default='OT', help='target feature in S or MS task')
@@ -49,18 +56,23 @@ def main():
     parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='location of model checkpoints')
 
     # forecasting task
-    parser.add_argument('--seq_len', type=int, default=96, help='input sequence length')
-    parser.add_argument('--label_len', type=int, default=48, help='start token length')
-    parser.add_argument('--pred_len', type=int, default=96, help='prediction sequence length')
+    # parser.add_argument('--seq_len', type=int, default=96, help='input sequence length')
+    # parser.add_argument('--label_len', type=int, default=48, help='start token length')
+    # parser.add_argument('--pred_len', type=int, default=96, help='prediction sequence length')
+
+    parser.add_argument('--seq_len', type=int, default=36, help='input sequence length')
+    parser.add_argument('--label_len', type=int, default=18, help='start token length')
+    parser.add_argument('--pred_len', type=int, default=60, help='prediction sequence length')
+
     # parser.add_argument('--cross_activation', type=str, default='tanh'
 
     # model define
-    parser.add_argument('--enc_in', type=int, default=7, help='encoder input size')
-    parser.add_argument('--dec_in', type=int, default=7, help='decoder input size')
-    parser.add_argument('--c_out', type=int, default=7, help='output size')
+    parser.add_argument('--enc_in', type=int, default=1, help='encoder input size')
+    parser.add_argument('--dec_in', type=int, default=1, help='decoder input size')
+    parser.add_argument('--c_out', type=int, default=1, help='output size')
     parser.add_argument('--d_model', type=int, default=512, help='dimension of model')
     parser.add_argument('--n_heads', type=int, default=8, help='num of heads')
-    parser.add_argument('--e_layers', type=int, default=2, help='num of encoder layers') #EDITED BY DAVID S
+    parser.add_argument('--e_layers', type=int, default=2, help='num of encoder layers')
     parser.add_argument('--d_layers', type=int, default=1, help='num of decoder layers')
     parser.add_argument('--d_ff', type=int, default=2048, help='dimension of fcn')
     parser.add_argument('--moving_avg', default=[24], help='window size of moving average')
@@ -72,15 +84,15 @@ def main():
     parser.add_argument('--embed', type=str, default='timeF',
                         help='time features encoding, options:[timeF, fixed, learned]')
     parser.add_argument('--activation', type=str, default='gelu', help='activation')
-    parser.add_argument('--output_attention', action='store_true', help='whether to output attention in ecoder')
+    parser.add_argument('--output_attention', action='store_true', help='whether to output attention in encoder')
     parser.add_argument('--do_predict', action='store_true', help='whether to predict unseen future data')
 
     # optimization
     parser.add_argument('--num_workers', type=int, default=10, help='data loader num workers')
-    parser.add_argument('--itr', type=int, default=3, help='experiments times')
+    parser.add_argument('--itr', type=int, default=5, help='experiments times')
     parser.add_argument('--train_epochs', type=int, default=10, help='train epochs')
     parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
-    parser.add_argument('--patience', type=int, default=3, help='early stopping patience')
+    parser.add_argument('--patience', type=int, default=2, help='early stopping patience')
     parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')
     parser.add_argument('--des', type=str, default='test', help='exp description')
     parser.add_argument('--loss', type=str, default='mse', help='loss function')
@@ -92,6 +104,7 @@ def main():
     parser.add_argument('--gpu', type=int, default=0, help='gpu')
     parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
     parser.add_argument('--devices', type=str, default='0,1', help='device ids of multi gpus')
+    parser.add_argument("-f", "--fff", help="a dummy argument to fool ipython", default="1")
 
     args = parser.parse_args()
 
@@ -105,12 +118,21 @@ def main():
 
     print('Args in experiment:')
     print(args)
+    print("Pre-Training is", pre_train)
 
     Exp = Exp_Main
 
+    encoder_loss_iteration = 9
+
     if args.is_training:
+        # for eni in range(encoder_loss_iteration):
+        #     # if eni == 0 or eni == 1 or eni == 2 or eni == 3 or eni == 4:
+        #     # if eni == 0:
+        #         break
+
         for ii in range(args.itr):
             # setting record of experiments
+            # eni after mode_select!!!
             setting = '{}_{}_{}_modes{}_{}_ft{}_sl{}_ll{}_pl{}_dm{}_nh{}_el{}_dl{}_df{}_fc{}_eb{}_dt{}_{}_{}'.format(
                 args.task_id,
                 args.model,
@@ -133,6 +155,10 @@ def main():
                 ii)
 
             exp = Exp(args)  # set experiments
+            if pre_train:
+                print('>>>>>>>start pre-training : {}>>>>>>>>>>>>>>>>>>>>>>>>>>'.format(setting))
+                exp.pre_train(setting)
+
             print('>>>>>>>start training : {}>>>>>>>>>>>>>>>>>>>>>>>>>>'.format(setting))
             exp.train(setting)
 
